dab1e7de2dc4a830b875fd3e5ab58c7c369c0b72
camille-louise k. mbayo <camille.mbayo@gmail.com>
2026-05-18 02:39:17 -0400

    error message added to media upload

--- app/api/media/route.ts
+++ app/api/media/route.ts
@@ -12,35 +12,40 @@ import { MediaType, MediaOwner } from "@/app/generated/prisma/client";
 // };
 
 export async function POST(request: NextRequest) {
-  const formData = await request.formData();
-  const file = formData.get("file") as File;
-  const type = formData.get("type") as MediaType;
-  const mediaOwner = formData.get("mediaOwner") as MediaOwner;
-  const ownerId = parseInt(formData.get("ownerId") as string);
+  try {
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+    const type = formData.get("type") as MediaType;
+    const mediaOwner = formData.get("mediaOwner") as MediaOwner;
+    const ownerId = parseInt(formData.get("ownerId") as string);
 
-  if (!file || !type ) { // mediaOwner and ownerId are optional for testing purposes
-    return Response.json({ error: "Missing required fields" }, { status: 400 });
+    if (!file || !type ) { // mediaOwner and ownerId are optional for testing purposes
+      return Response.json({ error: "Missing required fields" }, { status: 400 });
+    }
+
+    const url = await uploadFile(file);
+
+    const media = await prisma.media.create({
+      data: {
+        url,
+        type,
+        ...(mediaOwner && ownerId && {
+          attachments: {
+              create: {
+              mediaOwner,
+              ownerId,
+              },
+          },
+        }),
+      },
+      include: {
+        attachments: true,
+      },
+    });
+
+    return Response.json({ data: media }, { status: 201 });
+  } catch (error) {
+    console.error("Error uploading media:", error);
+    return Response.json({ error: "Failed to upload media" }, { status: 500 });
   }
-
-  const url = await uploadFile(file);
-
-  const media = await prisma.media.create({
-    data: {
-      url,
-      type,
-      ...(mediaOwner && ownerId && {
-        attachments: {
-            create: {
-            mediaOwner,
-            ownerId,
-            },
-        },
-      }),
-    },
-    include: {
-      attachments: true,
-    },
-  });
-
-  return Response.json({ data: media }, { status: 201 });
 }
