a0b8c05edd38eb87a34e15a8346604f86289f84f
camille-louise k. mbayo <camille.mbayo@gmail.com>
2026-05-18 02:50:07 -0400

    debugging to see what's going on

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,3 +6,6 @@ const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey); 
+
+console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("Service key exists:", !!process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY);
