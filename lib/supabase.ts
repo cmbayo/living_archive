c068dd8176bd2b706484178d1c8223f91dd6e88c
cmbayo <camille.mbayo@gmail.com>
2026-06-06 20:31:30 -0400

    fixing exposed service role key + rotate key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,9 @@
+import "server-only";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
-const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey); 
